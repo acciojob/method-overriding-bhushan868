ecc5c8d6fb309c1e30c6eb82b84a63289cb65a82
bhushan868 <bhushukarale643@gmail.com>
2023-05-21 17:58:39 +0530

    Zal ba ekdach

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,12 +3,14 @@ package com.driver;
 import com.sun.jdi.PathSearchingVirtualMachine;
 class A{
         void meth(){
+
             System.out.println("Invoking method from class A");
         }
     }
    class B extends A{
         void meth() {
-            System.out.println("Method is overridden in Extendend class B");
+
+            System.out.println("Method is overridden in Extended class B");
         }
 
     }
